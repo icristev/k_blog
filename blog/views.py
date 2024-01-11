38b841636f9b126aec140d8d9226885b51a06b37
Иван Кристев <icristev@mail.ru>
2024-01-11 21:20:00 +0300

    Start 3

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator
+
+
+def index(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 30)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    return render(request,
+                  'blog/index.html',
+                  {'posts': posts})
